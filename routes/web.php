a5f85548da94ca016bf3593652ae79d1665663a1
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-09 15:41:32 +0200

    fix(pint): format files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\TelegramController;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', static fn (): View => view('welcome'));
+
+Route::post('/telegram/webhook', [TelegramController::class, 'webhook'])
+    ->withoutMiddleware([VerifyCsrfToken::class]);
