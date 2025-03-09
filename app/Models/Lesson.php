4958bbe324afafde176399d07768ea9e9d7b4ccd
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-09 16:08:16 +0200

    fix(app): install barryvdh/laravel-ide-helper and add comments to models

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -8,6 +8,29 @@ use Database\Factories\LessonFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property int $day_of_week
+ * @property int $order
+ * @property bool|null $is_numerator
+ * @property \Carbon\CarbonImmutable|null $created_at
+ * @property \Carbon\CarbonImmutable|null $updated_at
+ *
+ * @method static \Database\Factories\LessonFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson whereDayOfWeek($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson whereIsNumerator($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson whereOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Lesson whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
 final class Lesson extends Model
 {
     /** @use HasFactory<LessonFactory> */
