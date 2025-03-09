c42b2c9aed7dad2ab42e6f20aac143a562dae8bb
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-09 16:40:01 +0200

    feat(telegram): create EscapeCharactersService

---
+++ app/Services/Lessons/Telegram/EscapeCharactersService.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Lessons\Telegram;
+
+use Illuminate\Support\Str;
+
+final class EscapeCharactersService
+{
+    public function handle(string $message): string
+    {
+        return Str::of($message)
+            ->replace('(', '\\(')
+            ->replace(')', '\\)')
+            ->replace('.', '\\.')
+            ->toString();
+    }
+}
