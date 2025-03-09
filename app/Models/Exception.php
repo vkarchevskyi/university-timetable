9987b9a80fdc185e5645e850f6876cd1e7acbc42
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-09 17:37:07 +0200

    feat(lesson): create Exception model with migration

---
+++ app/Models/Exception.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use App\Enums\LessonOrder;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string|null $name
+ * @property \Carbon\CarbonImmutable $date
+ * @property LessonOrder $order
+ * @property \Carbon\CarbonImmutable|null $created_at
+ * @property \Carbon\CarbonImmutable|null $updated_at
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exception newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exception newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exception query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exception whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exception whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exception whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exception whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Exception whereUpdatedAt($value)
+ *
+ * @mixin \Eloquent
+ */
+final class Exception extends Model
+{
+    protected $fillable = [
+        'datetime',
+        'name',
+        'order'
+    ];
+
+    protected $casts = [
+        'datetime' => 'immutable_datetime',
+        'order' => LessonOrder::class,
+    ];
+}
