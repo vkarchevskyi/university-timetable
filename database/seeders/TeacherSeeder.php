e56f88b44a2a3000f7e6d314b396b0670897d41e
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-13 10:14:03 +0200

    feat(teacher): create TeacherFactory and TeacherSeeder

---
+++ database/seeders/TeacherSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\Teacher;
+use Illuminate\Database\Seeder;
+
+final class TeacherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $teachers = [
+            [
+                'name' => 'Ізвалов О.В.',
+            ],
+            [
+                'name' => 'Паращук С.Д. ',
+            ],
+            [
+                'name' => 'Максимова О.П.',
+            ],
+            [
+                'name' => 'Сурков К.Ю.',
+            ],
+        ];
+
+        foreach ($teachers as $teacher) {
+            Teacher::factory()->state($teacher)->create();
+        }
+    }
+}
