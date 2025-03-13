1ed0148024bc2dbd216f00398078ccecfac27185
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-13 10:38:17 +0200

    fix(telegram): replace numbers with emoji

--- app/MessageIntegrations/Telegram/Formats/OrderMessageFormat.php
+++ app/MessageIntegrations/Telegram/Formats/OrderMessageFormat.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace App\MessageIntegrations\Telegram\Formats;
 
+use App\Enums\LessonOrder;
 use App\ValueObjects\LessonValueObject;
 
 final class OrderMessageFormat implements MessageFormatStrategy
 {
     public function format(LessonValueObject $lesson): string
     {
-        return "{$lesson->order->value}.";
+        return match ($lesson->order) {
+            LessonOrder::FIRST => ':one:',
+            LessonOrder::SECOND => ':two:',
+            LessonOrder::THIRD => ':three:',
+            LessonOrder::FOURTH => ':four:',
+            LessonOrder::FIFTH => ':five:',
+        };
     }
 }
