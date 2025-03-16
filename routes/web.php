d0a8717c9fd7e07cf3df4633995f045eab3fb2fc
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-16 21:45:29 +0200

    feat(google): create routes for google oauth

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,22 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\GoogleAuthController;
 use App\Http\Controllers\TelegramController;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static fn (): View => view('welcome'));
+Route::get('/', static fn (): View => view('welcome'))
+    ->name('homepage');
 
 Route::post('/telegram/webhook', [TelegramController::class, 'webhook'])
+    ->name('telegram.webhook')
     ->withoutMiddleware([VerifyCsrfToken::class]);
+
+Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect'])
+    ->name('auth.google');
+
+Route::get('/auth/google/callback', [GoogleAuthController::class, 'callback'])
+    ->name('auth.google.callback')
+    ->withoutMiddleware(VerifyCsrfToken::class);
