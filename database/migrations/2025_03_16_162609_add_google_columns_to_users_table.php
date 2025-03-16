d803dcd40c45652d6df7d667dbb78459ad15c587
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-16 20:32:58 +0200

    feat(classroom): add google columns to users table

---
+++ database/migrations/2025_03_16_162609_add_google_columns_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('google_id')->nullable();
+            $table->string('google_token')->nullable();
+            $table->string('google_refresh_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'google_id',
+                'google_token',
+                'google_refresh_token',
+            ]);
+        });
+    }
+};
