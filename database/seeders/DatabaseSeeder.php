41b162671abfe128f8aa62bfff536ad36f67ed8e
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-16 21:44:33 +0200

    refactor: Database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 final class DatabaseSeeder extends Seeder
@@ -15,11 +13,6 @@ final class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         $this->call(TeacherSeeder::class);
         $this->call(LessonSeeder::class);
         $this->call(ExceptionSeeder::class);
