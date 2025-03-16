4cf7ab7684f364812172fa4dd9ea56a7e466c8e1
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-16 21:51:03 +0200

    feat(google): create SaveGoogleUserDataService

---
+++ app/Services/Google/SaveGoogleUserDataService.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Google;
+
+use App\DataTransferObjects\GoogleUserData;
+use App\Models\User;
+use Illuminate\Container\Attributes\CurrentUser;
+use Laravel\Socialite\Two\GoogleProvider;
+
+final readonly class SaveGoogleUserDataService
+{
+    public function __construct(
+        private GoogleProvider $googleProvider,
+        #[CurrentUser]
+        private User $user,
+    ) {
+    }
+
+    public function handle(): void
+    {
+        $googleUser = $this->googleProvider->stateless()->user();
+
+        $this->user->update([
+            'google_id' => $googleUser->id,
+            'google_token' => $googleUser->token,
+            'google_refresh_token' => $googleUser->refreshToken,
+        ]);
+    }
+}
