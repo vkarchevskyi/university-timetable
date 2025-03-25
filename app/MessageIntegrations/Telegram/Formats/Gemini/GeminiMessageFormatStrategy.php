f46bd0fe6c5d093c6a516fa939d68784eaf5ae56
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-25 21:03:25 +0200

    feat(telegram): create GeminiMessageFormatStrategy and FormatGeminiMessageService to format messages

---
+++ app/MessageIntegrations/Telegram/Formats/Gemini/GeminiMessageFormatStrategy.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MessageIntegrations\Telegram\Formats\Gemini;
+
+interface GeminiMessageFormatStrategy
+{
+    public function format(string $message);
+}
