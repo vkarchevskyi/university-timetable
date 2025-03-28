a7d6e2423a9a694a8b1730d874ef706f41f8be10
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-28 16:50:14 +0200

    feat(classroom): use service classroom user in RefreshAuthTokenService

--- app/Services/Google/RefreshAuthTokenService.php
+++ app/Services/Google/RefreshAuthTokenService.php
@@ -5,23 +5,24 @@ declare(strict_types=1);
 namespace App\Services\Google;
 
 use App\Models\User;
-use Illuminate\Container\Attributes\CurrentUser;
+use Illuminate\Support\Facades\Config;
+use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\GoogleProvider;
 
 final readonly class RefreshAuthTokenService
 {
-    public function __construct(
-        private GoogleProvider $googleProvider,
-        #[CurrentUser]
-        private User $user,
-    ) {
-    }
-
     public function handle(): void
     {
-        $tokenData = $this->googleProvider->refreshToken($this->user->google_refresh_token);
+        /** @var GoogleProvider $googleProvider */
+        $googleProvider = Socialite::driver('google');
+        $classroomUserEmail = Config::string('services.google.classroom.email');
 
-        $this->user->update([
+        /** @var User $user */
+        $user = User::whereEmail($classroomUserEmail)->firstOrFail();
+
+        $tokenData = $googleProvider->refreshToken($user->google_refresh_token);
+
+        $user->update([
             'google_token' => $tokenData->token,
             'google_refresh_token' => $tokenData->refreshToken,
         ]);
