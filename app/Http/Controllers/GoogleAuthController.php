8921219c2fafe29c4e4f0c14eb7a019c0d84b38e
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-28 15:55:38 +0200

    fix(classroom): remove exception handling to GoogleAuthController

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\DataTransferObjects\GoogleUserData;
 use App\Services\Google\SaveGoogleUserDataService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Config;
 use Laravel\Socialite\Facades\Socialite;
-use Exception;
 
 final class GoogleAuthController extends Controller
 {
@@ -27,12 +25,8 @@ final class GoogleAuthController extends Controller
 
     public function callback(SaveGoogleUserDataService $saveGoogleUserDataService): JsonResponse
     {
-        try {
-            $saveGoogleUserDataService->handle();
+        $saveGoogleUserDataService->handle();
 
-            return new JsonResponse(['success' => 'Google Classroom connected successfully!']);
-        } catch (Exception $e) {
-            return new JsonResponse(['error' => 'Google authentication failed! ' . $e->getMessage()], 500);
-        }
+        return new JsonResponse(['success' => 'Google Classroom connected successfully!']);
     }
 }
