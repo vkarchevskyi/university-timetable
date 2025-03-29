874367d120758067e02309695a24cae258d8115c
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-03-29 17:25:58 +0200

    feat(courses): create CourseFactory

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Database\Factories\CourseFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 final class Course extends Model
 {
+    /** @use HasFactory<CourseFactory> */
+    use HasFactory;
+
     protected $fillable = [
         'title',
     ];
