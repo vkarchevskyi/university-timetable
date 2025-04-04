4aca0f91530ae700f183a3ba66d2bb7f1240a621
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-04 21:38:25 +0300

    feat(bot): add translation for bot

--- lang/uk/bot.php
+++ lang/uk/bot.php
@@ -5,5 +5,16 @@ declare(strict_types=1);
 return [
     'donate' => [
         'message' => 'Підтримай розробку бота! :link'
-    ]
+    ],
+
+    'banks' => [
+        'currency' => 'Валюта',
+        'buy' => 'Купівля',
+        'sell' => 'Продаж',
+
+        'mono' => 'Монобанк',
+        'privat' => 'Приватбанк',
+        'cash' => 'Готівковий',
+        'cashless' => 'Безготівковий',
+    ],
 ];
