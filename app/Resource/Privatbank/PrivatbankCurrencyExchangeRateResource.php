75c4299ef837078c4388c1528c85d5dad672afa8
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-04 19:48:29 +0300

    feat(privat): create PrivatbankCurrencyExchangeRateResource

---
+++ app/Resource/Privatbank/PrivatbankCurrencyExchangeRateResource.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Resource\Privatbank;
+
+final readonly class PrivatbankCurrencyExchangeRateResource
+{
+    public function __construct(
+        public string $currencyA,
+        public string $currencyB,
+        public float $rateSell,
+        public float $rateBuy,
+    ) {
+    }
+}
