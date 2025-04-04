886a5f2888546b5b133aae3f15d380962c3ac2db
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-04 18:41:06 +0300

    feat(mono): create MonobankApiError

---
+++ app/Exceptions/Banks/Monobank/MonobankApiError.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions\Banks\Monobank;
+
+use App\Exceptions\BusinessException;
+use Throwable;
+
+class MonobankApiError extends BusinessException
+{
+    /**
+     * @param array<string, string> $params
+     */
+    public function __construct(
+        string $message = 'exceptions.api.monobank',
+        array $params = [],
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
+        parent::__construct(__($message, $params), $code, $previous);
+    }
+}
