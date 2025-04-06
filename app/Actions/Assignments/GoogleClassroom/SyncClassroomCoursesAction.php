19b58e5396a4b43dc2bc083e142fa6fbd07e4882
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-06 13:42:10 +0300

    feat(classroom): create SyncClassroomCoursesAction

---
+++ app/Actions/Assignments/GoogleClassroom/SyncClassroomCoursesAction.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Assignments\GoogleClassroom;
+
+use App\Exceptions\Assignments\GoogleClassroom\AuthenticationException;
+use App\Models\ClassroomCourse;
+use App\Repositories\Assignments\GoogleClassroom\GoogleClassroomCoursesRepository;
+use Illuminate\Http\Client\ConnectionException;
+use JsonException;
+use RuntimeException;
+
+final readonly class SyncClassroomCoursesAction
+{
+    public function __construct(private GoogleClassroomCoursesRepository $classroomCoursesRepository)
+    {
+    }
+
+    /**
+     * @throws ConnectionException
+     * @throws JsonException
+     * @throws AuthenticationException
+     * @throws RuntimeException
+     */
+    public function handle(): void
+    {
+        $coursesData = [];
+        foreach ($this->classroomCoursesRepository->list() as $course) {
+            $coursesData[] = [
+                'classroom_id' => $course->classroomId,
+                'name' => $course->name,
+                'owner_id' => $course->ownerId,
+                'course_state' => $course->courseState,
+                'alternate_link' => $course->alternateLink,
+                'calendar_id' => $course->calendarId,
+            ];
+        }
+
+        ClassroomCourse::query()->upsert(
+            $coursesData,
+            uniqueBy: ['classroom_id'],
+            update: ['name', 'owner_id', 'course_state', 'alternate_link', 'calendar_id']
+        );
+    }
+}
