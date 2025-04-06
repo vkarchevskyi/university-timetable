6e76d98f606f2c0d01f725553375f50341f1bbe9
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-06 13:42:13 +0300

    feat(classroom): create AuthenticationException

---
+++ app/Exceptions/Assignments/GoogleClassroom/AuthenticationException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions\Assignments\GoogleClassroom;
+
+use App\Exceptions\BusinessException;
+
+final class AuthenticationException extends BusinessException
+{
+}
